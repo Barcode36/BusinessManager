12f8ec85c90d9232549fd06d48e403e824264bb9
Erik PC <Erik PC@DESKTOP-1C9O3ST>
2018-05-08 22:01:12 +0200

    Materials are done

--- src/classes/MngApi.java
+++ src/classes/MngApi.java
@@ -95,60 +95,6 @@ public class MngApi {
         newTime = new SimpleStringProperty(String.format("%dh %02dm", hours, minutes));
         
         return newTime;
-    }
-    
-    //perform any SQL statement and return result set
-    public static ResultSet performSQLQuery(User user, String query) {        
-        
-        // JDBC driver name and database URL
-        String JDBC_DRIVER = "org.mariadb.jdbc.Driver";
-        String DB_URL = "jdbc:mariadb://" + user.getAddress() + "/" + user.getDbName();
-
-        //  Database credentials
-        String USER = user.getName();
-        String PASS = user.getPass();
-
-
-        Connection conn = null;
-        Statement stmt = null;
-        ResultSet rs = null;
-        try {
-            
-            //STEP 2: Register JDBC driver
-            Class.forName("org.mariadb.jdbc.Driver");
-
-            //STEP 3: Open a connection
-
-            conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            //STEP 4: Execute a query
-            stmt = conn.createStatement();
-            
-            rs = stmt.executeQuery(query);            
-            
-            rs.close();
-        } catch (SQLException se) {
-            //Handle errors for JDBC
-            se.printStackTrace();
-        } catch (Exception e) {
-            //Handle errors for Class.forName
-            e.printStackTrace();
-        } finally {
-            //finally block used to close resources
-            try {
-                if (stmt != null)
-                    conn.close();
-            } catch (SQLException se) {
-            }// do nothing
-            try {
-                if (conn != null)
-                    conn.close();
-            } catch (SQLException se) {
-                se.printStackTrace();
-            }//end finally try
-        }//end try
-        
-    return rs;
-    }
-    
+    }    
 }
 
