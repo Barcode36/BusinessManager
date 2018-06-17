04d22f34268ddfd4ee280854d672b58d9112692f
Erik PC <Erik PC@DESKTOP-1C9O3ST>
2018-06-17 23:38:52 +0200

    new order controller in progress

--- src/controllers/orders/NewOrderController.java
+++ src/controllers/orders/NewOrderController.java
@@ -5,9 +5,18 @@
  */
 package controllers.orders;
 
+import classes.Order;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -16,9 +25,34 @@ import javafx.fxml.Initializable;
  */
 public class NewOrderController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    
+    @FXML
+    private Label label_orderID, label_weight, label_supportWeight, label_weightSum, label_quantity, label_buildTime, label_price, label_costs, label_profit;
+    
+    @FXML
+    private TextField txtField_customer, txtField_comment, txtField_price;
+    
+    @FXML
+    private Button btn_selectCustomer, btn_addObject, btn_removeSelected, btn_calculatePrices, btn_create, btn_cancel;
+    
+    @FXML
+    private RadioButton radioBtn_Sold, radioBtn_NotSold;
+    
+    @FXML
+    private DatePicker datePicker_dateCreated, datePicker_dueDate;
+    
+    @FXML
+    private TableView<Order> tv_selectedObjects;
+    
+    @FXML
+    private TableColumn<Order, Integer> col_objectID, col_quantity, col_buldTime, col_materialID;
+    
+    @FXML
+    private TableColumn<Order, String> col_objectName, col_printer, col_materialType, col_materialColor;
+    
+    @FXML
+    private TableColumn<Order, Double> col_weight, col_supportWeight, col_objectPrice,col_objectCosts;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
